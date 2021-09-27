1fb1df7663ad59416e7b9021320849a55b5a9b7c
iamssh <saadhasan620@gmail.com>
2021-09-27 18:18:41 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
-# StoreIt-cloud-storage
+# StoreIt Cloud Storage
 
-To run the project locally:
+StoreIt is a web application that allows users to store their files on the cloud.
+
+It is built using **NodeJS** on the back-end and simple **HTML and CSS** on the front-end.
+
+## How It Works
+
+Click on the **Sign Up** button to create an account (or **Login**, if you already have one).
+
+<ins>_Landing Page_</ins>
+<img src="./screenshots/landing.png" alt="landing page">
+
+Create your account on the **Register** page and click Sign Up. You'll be taken to the page where you can upload your files.
+
+<ins>_Register_</ins>
+<img src="./screenshots/register.png" alt="register page">
+
+<ins>_Login_</ins>
+<img src="./screenshots/login.png" alt="login page">
+
+<ins>_Upload Page_</ins>
+<img src="./screenshots/upload_page.png" alt="upload page">
+
+Select any file and click on **Submit**
+
+Voila! You have successfully uploaded the file to the cloud 😄
+
+<ins>_Upload Page With Files_</ins>
+<img src="./screenshots/list.png" alt="upload page with files">
+
+You can download or delete it any time you want.
+
+## Run Locally
+
+To run the project locally, ensure that you have `node` and `mongo` installed on your system, then follow the below steps:
 
 1. Clone the repo.
-2. Run npm install.
-3. Set the credentials for the DB.
-4. Visit localhost:8080. 
-    
-Thats it!
+2. Run `npm install` in the root directory.
+3. Set the credentials for the DB in `index.js` file.
+4. Run `node index.js` in the root directory.
+5. Visit `localhost:8080` in your browser.
+
+Thats it! 😊😊
--- index.js
+++ index.js
@@ -1,171 +1,161 @@
-const PassportLocalMongoose = require('passport-local-mongoose');
-const GridFsStorage = require('multer-gridfs-storage');
-const methodOverride = require('method-override');
-const LocalStrategy = require('passport-local');
-const bodyParser = require('body-parser');
+const PassportLocalMongoose = require("passport-local-mongoose");
+const GridFsStorage = require("multer-gridfs-storage");
+const methodOverride = require("method-override");
+const LocalStrategy = require("passport-local");
+const bodyParser = require("body-parser");
 const User = require("./models/User");
-const Grid = require('gridfs-stream');
-var flash = require('connect-flash');
-const mongoose = require('mongoose');
-var passport = require('passport');
-const express = require('express');
-const multer = require('multer');
-var asyc = require('async');
+const Grid = require("gridfs-stream");
+var flash = require("connect-flash");
+const mongoose = require("mongoose");
+var passport = require("passport");
+const express = require("express");
+const multer = require("multer");
+var asyc = require("async");
 const app = express();
 
 // Middleware
-app.use(require("express-session")({
-  secret : "Hello there!",
-  resave : false ,
-  saveUninitialized : false
-}))
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(
+  require("express-session")({
+    secret: "Hello there!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use( express.static( "public" ) );
-app.use(function(req,res,next){
-    res.locals.currUser = req.user;
-    // res.locals.message = req.flash("info");
-    // res.locals.mess = req.flash("err");
-    next();
-})
+app.use(express.static("public"));
+app.use(function (req, res, next) {
+  res.locals.currUser = req.user;
+  next();
+});
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-const mongoURI = '';
+const mongoURI = "mongodb://localhost:27017/test_db";
 mongoose.connect(mongoURI);
 const conn = mongoose.createConnection(mongoURI);
 
 let gfs;
 let loggedInUser;
 
-conn.once('open', () => {
+conn.once("open", () => {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+  gfs.collection("uploads");
 });
-app.get('/',function(req,res){
-  res.render('home');
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 //Auth Routes
-app.get('/register',function(req,res){
-  res.render('register');
-})
-app.post('/register',function(req,res){
-
-  User.register(new User({username:req.body.username}),req.body.password,function(err,user){
-    if(err){
-      console.log(err);
-      return res.render('register');
-    }
-    passport.authenticate('local')(req,res,function(){      
-        res.redirect('/up/'+req.body.username);
-    })
-   })
- });
- app.get('/login',function(req,res){
-  res.render('login');
-})
-app.post('/login',
-function(req,res){
-  passport.authenticate('local')(req,res,function(){   
-    //loggedInUser = req.body.username;
-    res.redirect('/up/'+req.body.username);
-  })
-}
-
-);
-app.get('/logout',function(req,res){
-  req.logout();
-  // req.flash('err',"Logged you out");
-  res.redirect("/");
-})
-
-app.get('/up/:id',isLoggedIn,fetchDetails,(req, res) => {
-  //if(res.locals.filesarray.length !==0 ){
-    // var fetchDetail = fetchDetails(); 
-    // fetchDetail.then({
-      // function(filesarray){
-        res.render('index', {files:res.locals.userfiles});
-      // }
-    // })
-    
-  //}
-    
+app.get("/register", function (req, res) {
+  res.render("register");
 });
-//Auth Routes end
+app.post("/register", function (req, res) {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/up/" + req.body.username);
+      });
+    }
+  );
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.post("/login", function (req, res) {
+  passport.authenticate("local")(req, res, function () {
+    res.redirect("/up/" + req.body.username);
+  });
+});
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
+app.get("/up/:id", isLoggedIn, fetchDetails, (req, res) => {
+  res.render("index", { files: res.locals.userfiles });
+});
 
 // @route POST /upload
 // @desc  Uploads file to DB
-// ************    Insert authentication middleware in this route!   ************
-app.post('/upload', (req, res) => {
+app.post("/upload", (req, res) => {
   const storage = new GridFsStorage({
     url: mongoURI,
     file: (req, file) => {
-      // fileName = file.originalname;
-    // var q = newFile.split(' ').join('_');
-      return  { filename: file.originalname.split(' ').join('_'), bucketName: 'uploads' };
-    }
+      return {
+        filename: file.originalname.split(" ").join("_"),
+        bucketName: "uploads",
+      };
+    },
   });
-  
+
   let upload = multer({
-    storage: storage
-  }).single('file');
-  
-  
-  upload(req,res, (err) => {
-    if(err){
-         res.json({error_code:1,err_desc:err});
-         return;
+    storage: storage,
+  }).single("file");
+
+  upload(req, res, (err) => {
+    if (err) {
+      res.json({ error_code: 1, err_desc: err });
+      return;
     }
     var newFile = req.file.filename;
-    var q = newFile.split(' ').join('_');
+    var q = newFile.split(" ").join("_");
     loggedInUser.file.push(q);
-    User.findByIdAndUpdate({_id: loggedInUser.id}, {$set: {file: loggedInUser.file}}).then((updatedDoc) => {})
+    User.findByIdAndUpdate(
+      { _id: loggedInUser.id },
+      { $set: { file: loggedInUser.file } }
+    ).then((updatedDoc) => {});
     console.log(q);
-    res.redirect('/up/'+loggedInUser.username);
+    res.redirect("/up/" + loggedInUser.username);
   });
-  
 });
 
 // @desc Download file with name 'filename'
-app.get('/file/:filename', (req, res) => {
-  gfs.collection('uploads'); //set collection name to lookup into
+app.get("/file/:filename", (req, res) => {
+  gfs.collection("uploads"); //set collection name to lookup into
 
   /** First check if file exists */
-  gfs.files.find({filename: req.params.filename}).toArray(function(err, files){
-      if(!files || files.length === 0){
-          return res.status(404).json({
-              responseCode: 1,
-              responseMessage: "error"
-          });
+  gfs.files
+    .find({ filename: req.params.filename })
+    .toArray(function (err, files) {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          responseCode: 1,
+          responseMessage: "error",
+        });
       }
       // create read stream
       var readstream = gfs.createReadStream({
-          filename: files[0].filename,
-          root: "uploads"
+        filename: files[0].filename,
+        root: "uploads",
       });
-      // set the proper content type 
-      res.set('Content-Type', files[0].contentType)
+      // set the proper content type
+      res.set("Content-Type", files[0].contentType);
       // Return response
       return readstream.pipe(res);
-  });
+    });
 });
 
 // @route GET /files
 // @desc  Display all files in JSON
-app.get('/files', (req, res) => {
+app.get("/files", (req, res) => {
   gfs.files.find().toArray((err, files) => {
     // Check if files
     if (!files || files.length === 0) {
       return res.status(404).json({
-        err: 'No files exist'
+        err: "No files exist",
       });
     }
 
@@ -176,12 +166,12 @@ app.get('/files', (req, res) => {
 
 // @route GET /files/:filename
 // @desc  Display single file object
-app.get('/files/:filename', (req, res) => {
+app.get("/files/:filename", (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: "No file exists",
       });
     }
     // File exists
@@ -191,76 +181,75 @@ app.get('/files/:filename', (req, res) => {
 
 // @route DELETE /files/:id
 // @desc  Delete file
-app.delete('/files/:id', (req, res) => {
-  gfs.files.remove({ filename : req.params.id}, (err, gridStore) => {
+app.delete("/files/:id", (req, res) => {
+  gfs.files.remove({ filename: req.params.id }, (err, gridStore) => {
     if (err) {
       return res.status(404).json({ err: err });
-    }   
+    }
     // 1. Delete the filename from user's file array and,
-    var index = req.user.file.indexOf(req.params.id)
+    var index = req.user.file.indexOf(req.params.id);
 
-    if(index>-1){
-      loggedInUser.file.splice(index,1);
+    if (index > -1) {
+      loggedInUser.file.splice(index, 1);
     }
 
-    User.findByIdAndUpdate({_id: loggedInUser.id}, {$set: {file: loggedInUser.file}}).then((updatedDoc) => {})    
-    
-    res.redirect('/up/' + loggedInUser.username);
+    User.findByIdAndUpdate(
+      { _id: loggedInUser.id },
+      { $set: { file: loggedInUser.file } }
+    ).then((updatedDoc) => {});
+
+    res.redirect("/up/" + loggedInUser.username);
   });
 });
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-      return next();
-    }else{
-      // req.flash('info',"Please login first!");
-      // req.flash('err',"Invalid username/password");
-      res.redirect("/login");
-    }
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
 }
-async function fetchDetails(req,res,next){
-    User.findOne({username : req.params.id}, function(err,user){
-      if (user !== null)
-      {
-        loggedInUser = user;
-      }
-      if(err) res.json(err);
-      else{      
-        if(user == null){
-          return res.render('index', { files: false });
-        }else{
-          var userFilesArray = user.file;
-          if(userFilesArray.length!= 0){  
+async function fetchDetails(req, res, next) {
+  User.findOne({ username: req.params.id }, function (err, user) {
+    if (user !== null) {
+      loggedInUser = user;
+    }
+    if (err) res.json(err);
+    else {
+      if (user == null) {
+        return res.render("index", { files: false });
+      } else {
+        var userFilesArray = user.file;
+        if (userFilesArray.length != 0) {
           let filesarray = [];
-          function storedata(){
-            return new Promise((resolve,reject)=>{
-              userFilesArray.forEach(function(element){
-                gfs.files.findOne({filename : element},function(err, found){
+          function storedata() {
+            return new Promise((resolve, reject) => {
+              userFilesArray.forEach(function (element) {
+                gfs.files.findOne({ filename: element }, function (err, found) {
                   //if(found !== null)
-                    filesarray.push(found);
-                  if(filesarray.length == userFilesArray.length)
+                  filesarray.push(found);
+                  if (filesarray.length == userFilesArray.length)
                     resolve(filesarray);
                 });
-              })
-            })
+              });
+            });
           }
-          function loadPage(){
+          function loadPage() {
             res.locals.userfiles = filesarray;
             console.log(filesarray);
             next();
           }
-          async function init(){
+          async function init() {
             await storedata();
             loadPage();
           }
           init();
-          }else{
-            console.log("no here")
-            return next();
-          }
+        } else {
+          console.log("no here");
+          return next();
         }
-      };
-    });
-
+      }
+    }
+  });
 }
 const port = 8080;
 app.listen(port, () => console.log(`Server started on port ${port}`));
